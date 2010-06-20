0b9df08760e07ffa6de6dca6c46bfa678e8ba50b
Reed O'Brien <reed@koansys.com>
2010-06-20 06:13:11 -0400

    create some really commonly used things.
    It is already useful and has let me pull a bunch of code out from that other library.

--- lumin/__init__.py
+++ lumin/__init__.py
@@ -1,16 +1,30 @@
 import re
 import unicodedata
 
+import pymongo
 from pymongo.errors import DuplicateKeyError
+from gridfs import GridFS 
 
-def insert_doc(collection, document, key='url_id', safe=True):
+#import memcache
+
+from repoze.bfg.security import Allow
+from repoze.bfg.security import Everyone
+
+from repoze.bfg.settings import get_settings
+
+from lumin.son import ColanderNullTransformer
+
+
+def insert_doc(collection, document, title_or_id, key='url_id', safe=True):
     """
     TODO: Insert a doc with a suffixed id if necessary as our unique id, and return
     TODO: prolly not ready for use yet. I just put a sanitized copy of this here.
     TODO: but it needs more though.
     """
-    suffix = 1
-    url_id = document[key]
+    suffix=0
+    collection.ensure_index(key, unique=True)
+    url_id = normalize(title_or_id)
+    document[key] = url_id
     while True:
         ## TODO: possible perfomance penalty
         ## If there are many of a given duplicate id
@@ -18,20 +32,32 @@ def insert_doc(collection, document, key='url_id', safe=True):
         ## deal. 
         ## TODO: add to api docs
         try:
-            collection.ensure_index(key, unique=True)
             oid = collection.insert(document, safe=safe)
-            return url_id
+            return document[key]
         except DuplicateKeyError, e:
+            suffix+=1
             url_id_suffixed = u','.join([url_id,
                                          unicode(suffix)])
             document[key] = url_id_suffixed
-            suffix+=1
-            return url_id_suffixed
 
 def normalize(title):
     """
     make url ready string/id from a title
     """
-    url_safe = unicodedata.normalize('NFKD', title).encode('ascii', 'ignore')
-    url_safe = unicode(re.sub('[^\w\s-]', '', url_safe).strip().lower())
+    url_safer = unicodedata.normalize('NFKD', title).encode('ascii', 'ignore')
+    url_safe = unicode(re.sub('[^\w\s-]', '', url_safer).strip().lower())
     return re.sub('[-\s]+', '-', url_safe)
+
+
+
+class RootFactory(object):
+    __acl__ = [ (Allow, Everyone, 'view'),]
+    def __init__(self, request, collection=None):
+        settings = get_settings()
+        self.db = pymongo.Connection.from_uri(
+            uri=settings['db_uri'])[settings['db_name']]
+        self.db.add_son_manipulator(ColanderNullTransformer())
+        self.fs = GridFS(self.db)
+        #self.mc = memcache.Client(settings['mc_host'])
+
+
--- lumin/son.py
+++ lumin/son.py
@@ -2,6 +2,8 @@ from pymongo.son_manipulator import SONManipulator
 
 import colander
 
+SENTINEL = {u'_type': u'colander.null'}
+
 class ColanderNullTransformer(SONManipulator):
     """Added to the db at load time in (at the time of this writing)
     run:get_root
@@ -11,7 +13,7 @@ class ColanderNullTransformer(SONManipulator):
     def transform_incoming(self, son, collection):
         for (k, v) in son.items():
             if v is colander.null:
-                son[k] = {'_type': 'colander.null'}
+                son[k] = SENTINEL
                 continue
             if isinstance(v, dict):
                 self.recursive_in(v)
@@ -26,15 +28,15 @@ class ColanderNullTransformer(SONManipulator):
     def recursive_in(self, subson):
         for (k, v) in subson.items():
             if v is colander.null:
-                subson[k] = {'_type': 'colander.null'}
+                subson[k] = SENTINEL
                 continue
-            if isinstance(v, dict):
+            if isinstance(v, dict) and v != SENTINEL:
                 for (key, value) in v.items():
                     self.recursive_in(value)
 
     def recursive_out(self, subson):
         for (k, v) in subson.items():
             if isinstance(v, dict):
-                if "_type" in v and v['_type'] == 'colander.null':
+                if v == SENTINEL:
                     subson[k] = colander.null
                 self.recursive_out(v)
