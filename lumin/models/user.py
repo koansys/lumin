6f9d46041893ac571faee03351c18dce7346a7a3
Reed O'Brien <reed@koansys.com>
2010-07-22 03:08:11 -0400

    add user model and security package

---
+++ lumin/models/user.py
@@ -0,0 +1,82 @@
+import colander
+from colander import Float
+from colander import SchemaNode
+from colander import String
+
+from repoze.bfg.security import authenticated_userid
+from repoze.bfg.security import Allow
+from repoze.bfg.security import Everyone
+
+from lumin import RootFactory
+
+
+class UserSchema(colander.MappingSchema):
+    username = SchemaNode(String(),
+                          title="User",
+                          description="The name of the participant")
+    given_name = SchemaNode(String(), missing='',
+                            title="Given Name")
+    surname = SchemaNode(String(), missing='',
+                         title="Surname")
+    street_address = SchemaNode(String(), missing='',
+                                title="Street Address",
+                                description='Address info (number, street, unit)')
+    locality = SchemaNode(String(), missing='',
+                      title='City',
+                      description="City or township name")
+    ## TODO: There must be an ISO list for this
+    region = SchemaNode(String(), missing='',
+                       title='Locality',
+                       description='State, Province, Township or equivalent')
+    postal_code = SchemaNode(String(), missing='',
+                             title='Postal Code',
+                             description='ZIP or postal code')
+    ## TODO: make this oneOf ISO countries
+    country_name = SchemaNode(String(), missing='',
+                              title='Country',
+                              description='Country')
+    telephone = SchemaNode(String(), missing='',
+                       title='Telephone Number')
+    fax = SchemaNode(String(), missing='',
+                     title='Fax number')
+    website_url = SchemaNode(String(), missing='',
+                             title='Website URL',
+                             description='I.e. http://example.com')
+    latitude = SchemaNode(Float(), missing=colander.null,
+                          title='Latitude')
+    longitude = SchemaNode(Float(), missing=colander.null,
+                           title='Longitude')
+
+
+                         
+class User(RootFactory):
+
+    __acl__ = [
+        (Allow, Everyone, 'view'), ## Really?
+        (Allow, Everyone, ('add')),
+        (Allow, 'group:users', ('add', 'edit')),
+        (Allow, 'group:managers', ('add', 'edit', 'delete')),
+        ]
+
+    __parent__ = __collection__ = 'users'
+    
+    def __init__(self, request):
+        super(User, self).__init__(request)
+        self.request = request
+        self.environ = request.environ
+        self.collection = self.db[self.__collection__]
+        self.logged_in = authenticated_userid(request)
+        self.user_id = request.matchdict.get('url_id')
+        if self.user_id:
+            cursor = self.collection.find(
+                {'url_id' : self.participant_id}
+                )
+            try:
+                self.user = cursor.next()
+            except StopIteration:
+                self.user = None
+            
+            assert cursor.count() == 1
+        self.schema = UserSchema()
+        
+        
---
+++ lumin/security.py
@@ -0,0 +1,83 @@
+from datetime import datetime
+from hashlib import sha224
+
+from webob.exc import HTTPFound
+
+from repoze.bfg.chameleon_zpt import get_template
+from repoze.bfg.security import authenticated_userid
+from repoze.bfg.security import remember
+from repoze.bfg.security import forget
+from repoze.bfg.url import route_url
+
+
+class GroupFinder:
+    def __init__(self, collection_name='users'):
+        self.collection_name = collection_name
+
+    def __call__(self, userid, request):
+        try:
+            user = request.root.db[self.collection_name].find(
+                {'username' : userid}).next()
+        except StopIteration:
+            user = None
+        if user:
+            return user['groups']
+groupfinder = GroupFinder()
+
+
+class Login:
+    def __init__(self, api, collection_name='users'):
+        self.api = get_template(api)
+        self.collection_name = collection_name
+        
+    def __call__(self, request):
+        login_url = route_url('login', request)
+        referrer = request.url
+        if referrer == login_url:
+            referrer = '/'
+        came_from = request.params.get('came_from', referrer)
+        message = login = password = ''
+        if 'form.submitted' in request.params:
+            login = request.params['login']
+            password = request.params['password']
+            try:
+                user = request.root.db[self.collection_name].find(
+                    {'username' : login}).next()
+            except StopIteration:
+                user = None
+            if user:
+                if sha224(password).hexdigest() == user['password']:
+                    headers = remember(request, login)
+                    
+                    request.root.db[self.collection_name].update(
+                        {"_id" : user['_id']}, 
+                        {"$set" : {'last_login' : datetime.now()}}
+                         )
+                    return HTTPFound(location=came_from,
+                                     headers=headers)
+            message = 'Login failed'
+
+        return dict(
+            api = self.api,
+            came_from = came_from,
+            logged_in = authenticated_userid(request),
+            login = login,
+            message = message,
+            password = password,
+            section = 'login',
+            title = 'ravel | Log in',
+            url = request.application_url + '/login',
+            )
+login = Login()
+
+
+class Logout:
+    def __init__(self, view_name='home'):
+        self.view_name = view_name
+    
+    def __call__(self, request):
+        headers = forget(request)
+        return HTTPFound(location=route_url(self.view_name, request),
+                         headers = headers)
+logout = Logout()
+
