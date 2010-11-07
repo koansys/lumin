5d6f11753ccfc83bbc1e6f1bc39a81dab34f6180
Reed O'Brien <reed@koansys.com>
2010-11-07 14:11:16 -0500

    Updated User factory

--- lumin/models/user.py
+++ lumin/models/user.py
@@ -1,13 +1,40 @@
+from webob.exc import HTTPNotFound
+from webob.exc import HTTPInternalServerError
+
 import colander
 from colander import Float
 from colander import SchemaNode
 from colander import String
+
+import deform
 
 from pyramid.security import authenticated_userid
 from pyramid.security import Allow
 from pyramid.security import Everyone
 
 from lumin import RootFactory
+from lumin.util import reset
+
+
+email_widget = deform.widget.CheckedInputWidget(
+    subject="Email",
+    confirm_subject="Confirm Email",
+    size=40
+    )
+
+class EmailSchema(colander.MappingSchema):
+    email = SchemaNode(String(),
+                       title="email",
+                       description='Type your email address and confirm it',
+                       validator=colander.Email(),
+                       widget=email_widget)
+
+
+class PasswordSchema(colander.MappingSchema):
+    password = SchemaNode(String(),
+                          validator=colander.Length(min=6),
+                          widget = deform.widget.CheckedPasswordWidget(size=40),
+                          description="Type your password and confirm it")
 
 
 class UserSchema(colander.MappingSchema):
@@ -18,37 +45,19 @@ class UserSchema(colander.MappingSchema):
                             title="Given Name")
     surname = SchemaNode(String(), missing='',
                          title="Surname")
-    street_address = SchemaNode(String(), missing='',
-                                title="Street Address",
-                                description='Address info (number, street, unit)')
-    locality = SchemaNode(String(), missing='',
-                      title='City',
-                      description="City or township name")
-    ## TODO: There must be an ISO list for this
-    region = SchemaNode(String(), missing='',
-                       title='Locality',
-                       description='State, Province, Township or equivalent')
-    postal_code = SchemaNode(String(), missing='',
-                             title='Postal Code',
-                             description='ZIP or postal code')
-    ## TODO: make this oneOf ISO countries
-    country_name = SchemaNode(String(), missing='',
-                              title='Country',
-                              description='Country')
-    telephone = SchemaNode(String(), missing='',
-                       title='Telephone Number')
-    fax = SchemaNode(String(), missing='',
-                     title='Fax number')
-    website_url = SchemaNode(String(), missing='',
-                             title='Website URL',
-                             description='I.e. http://example.com')
-    latitude = SchemaNode(Float(), missing=colander.null,
-                          title='Latitude')
-    longitude = SchemaNode(Float(), missing=colander.null,
-                           title='Longitude')
+    email = EmailSchema()
+    password = PasswordSchema()
+    # email = SchemaNode(String(),
+    #                    title="email",
+    #                    description='Type your email address and confirm it',
+    #                    validator=colander.Email(),
+    #                    widget=email_widget)
+    # password = SchemaNode(String(),
+    #                       validator=colander.Length(min=6),
+    #                       widget = deform.widget.CheckedPasswordWidget(size=40),
+    #                       description="Type your password and confirm it")
 
 
-                         
 class User(RootFactory):
 
     __acl__ = [
@@ -59,7 +68,7 @@ class User(RootFactory):
         ]
 
     __parent__ = __collection__ = 'users'
-    
+
     def __init__(self, request):
         super(User, self).__init__(request)
         self.request = request
@@ -69,14 +78,26 @@ class User(RootFactory):
         self.user_id = request.matchdict.get('url_id')
         if self.user_id:
             cursor = self.collection.find(
-                {'url_id' : self.participant_id}
+                {'__uid__' : self.user_id}
                 )
             try:
+                assert cursor.count() < 2
                 self.user = cursor.next()
             except StopIteration:
-                self.user = None
-            
-            assert cursor.count() == 1
+                raise HTTPNotFound
+            except AssertionError:
+                raise HTTPInternalServerError
+
         self.schema = UserSchema()
-        
-        
+
+    def add_form(self):
+        buttons = (deform.form.Button(name = "submit",
+                                        title = "Create user"
+                                        ),
+                   reset)
+        form = deform.Form(self.schema, buttons=buttons)
+        resources = form.get_widget_resources()
+        return (form, resources)
+
+    def insert(self, doc):
+        pass
