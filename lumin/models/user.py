0828411c6b2f05a4a353156ec5a7cec4ec7e45c9
Reed O'Brien <reed@koansys.com>
2010-11-07 22:10:59 -0500

    correct NotFound view to get the tempalted one

--- lumin/models/user.py
+++ lumin/models/user.py
@@ -1,4 +1,3 @@
-from webob.exc import HTTPNotFound
 from webob.exc import HTTPInternalServerError
 
 import colander
@@ -8,6 +7,7 @@ from colander import String
 
 import deform
 
+from pyramid.exceptions import NotFound
 from pyramid.security import authenticated_userid
 from pyramid.security import Allow
 from pyramid.security import Everyone
@@ -67,6 +67,34 @@ class UserSchema(colander.MappingSchema):
                           widget = deform.widget.CheckedPasswordWidget(size=40),
                           description="Type your password and confirm it")
 
+    street_address = SchemaNode(String(), missing='',
+                                title="Street Address",
+                                description='Address info (number, street, unit)')
+    locality = SchemaNode(String(), missing='',
+                      title='City',
+                      description="City or township name")
+    ## TODO: There must be an ISO list for this
+    region = SchemaNode(String(), missing='',
+                       title='Locality',
+                       description='State, Province, Township or equivalent')
+    postal_code = SchemaNode(String(), missing='',
+                             title='Postal Code',
+                             description='ZIP or postal code')
+    ## TODO: make this oneOf ISO countries
+    country_name = SchemaNode(String(), missing='',
+                              title='Country',
+                              description='Country')
+    telephone = SchemaNode(String(), missing='',
+                       title='Telephone Number')
+    fax = SchemaNode(String(), missing='',
+                     title='Fax number')
+    website_url = SchemaNode(String(), missing='',
+                             title='Website URL',
+                             description='I.e. http://example.com')
+    latitude = SchemaNode(Float(), missing=colander.null,
+                          title='Latitude')
+    longitude = SchemaNode(Float(), missing=colander.null,
+                           title='Longitude')
 
 class User(RootFactory):
 
