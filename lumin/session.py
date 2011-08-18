aee715e81b6ede01282cb68ba32a90da64f8cae9
Reed O'Brien <reed@koansys.com>
2011-08-18 01:09:33 -0400

    add mongodb session storage and tests
    https://www.pivotaltracker.com/story/show/16682993

---
+++ lumin/session.py
@@ -0,0 +1,209 @@
+import os  # pragma: no cover
+import time   # pragma: no cover
+
+from pymongo.objectid import ObjectId   # pragma: no cover
+
+from zope.interface import implements  # pragma: no cover
+
+from pyramid.interfaces import ISession  # pragma: no cover
+from pyramid.session import manage_accessed  # pragma: no cover
+from pyramid.session import signed_deserialize  # pragma: no cover
+from pyramid.session import signed_serialize  # pragma: no cover
+
+
+def LuminSessionFactoryConfig(
+    secret,
+    timeout=1200,
+    cookie_name='lumin_session',
+    cookie_max_age=None,
+    cookie_path='/',
+    cookie_domain=None,
+    cookie_secure=False,
+    cookie_httponly=False,
+    cookie_on_exception=True,
+    ):
+    """
+    Parameters:
+
+    ``secret``
+      A string which is used to sign the cookie.
+
+    ``timeout``
+      A number of seconds of inactivity before a session times out.
+
+    ``cookie_name``
+      The name of the cookie used for sessioning.  Default: ``session``.
+
+    ``cookie_max_age``
+      The maximum age of the cookie used for sessioning (in seconds).
+      Default: ``None`` (browser scope).
+
+    ``cookie_path``
+      The path used for the session cookie.  Default: ``/``.
+
+    ``cookie_domain``
+      The domain used for the session cookie.  Default: ``None`` (no domain).
+
+    ``cookie_secure``
+      The 'secure' flag of the session cookie.  Default: ``False``.
+
+    ``cookie_httponly``
+      The 'httpOnly' flag of the session cookie.  Default: ``False``.
+
+    ``cookie_on_exception``
+      If ``True``, set a session cookie even if an exception occurs
+      while rendering a view.  Default: ``True``.
+
+    You can set the session collection name by adding a
+    ``lumin.session.collection`` key/value to the configuration .ini
+    """
+
+    class LuminSessionFactory(dict):
+
+        implements(ISession)
+
+        # configuration parameters
+        _cookie_name = cookie_name
+        _cookie_max_age = cookie_max_age
+        _cookie_path = cookie_path
+        _cookie_domain = cookie_domain
+        _cookie_secure = cookie_secure
+        _cookie_httponly = cookie_httponly
+        _cookie_on_exception = cookie_on_exception
+        _secret = secret
+        _timeout = timeout
+
+        # dirty flag
+        _dirty = False
+
+        def __init__(self, request):
+            self.request = request
+            self.collection = request.registry.settings.get(
+                "lumin.session.collection",
+                "lumin.sessions")
+            self.db = self.request.db
+
+            now = time.time()
+            created = accessed = now
+            new = True
+            value = None
+            state = self._new_session()
+            cookieval = request.cookies.get(self._cookie_name)
+
+            if cookieval is not None:
+                try:
+                    value = signed_deserialize(cookieval, self._secret)
+
+                except ValueError:
+                    value = None
+
+            if value is not None:
+                accessed, created, oid = value
+                new = False
+                state = self.db[self.collection].find_one({'_id': oid})
+                if not state:  # pragma: no cover
+                    state = self._new_session()
+                if now - accessed > self._timeout:
+                    if state:
+                        self.invalidate({'_id': state['_id']})
+                    state = self._new_session()
+
+            self.created = created
+            self.accessed = accessed
+            self.new = new
+            dict.__init__(self, state)
+
+        def _new_session(self):
+            return {'_id': ObjectId()}
+
+        # ISession methods
+        def changed(self):
+            return self._dirty
+
+        def invalidate(self, spec=None):
+            if not spec:
+                spec = {'_id': self['_id']}
+            self.db[self.collection].remove(spec)
+            self.clear()
+            # XXX probably needs to unset cookie
+
+        def save(self):
+            self.db[self.collection].save(self)
+
+        # non-modifying dictionary methods
+        get = manage_accessed(dict.get)
+        __getitem__ = manage_accessed(dict.__getitem__)
+        items = manage_accessed(dict.items)
+        iteritems = manage_accessed(dict.iteritems)
+        values = manage_accessed(dict.values)
+        itervalues = manage_accessed(dict.itervalues)
+        keys = manage_accessed(dict.keys)
+        iterkeys = manage_accessed(dict.iterkeys)
+        __contains__ = manage_accessed(dict.__contains__)
+        has_key = manage_accessed(dict.has_key)
+        __len__ = manage_accessed(dict.__len__)
+        __iter__ = manage_accessed(dict.__iter__)
+
+        # modifying dictionary methods
+        clear = manage_accessed(dict.clear)
+        update = manage_accessed(dict.update)
+        setdefault = manage_accessed(dict.setdefault)
+        pop = manage_accessed(dict.pop)
+        popitem = manage_accessed(dict.popitem)
+        __setitem__ = manage_accessed(dict.__setitem__)
+        __delitem__ = manage_accessed(dict.__delitem__)
+
+        # flash API methods
+        @manage_accessed
+        def flash(self, msg, queue='', allow_duplicate=True):
+            storage = self.setdefault('_f_' + queue, [])
+            if allow_duplicate or (msg not in storage):
+                storage.append(msg)
+
+        @manage_accessed
+        def pop_flash(self, queue=''):
+            storage = self.pop('_f_' + queue, [])
+            return storage
+
+        @manage_accessed
+        def peek_flash(self, queue=''):
+            storage = self.get('_f_' + queue, [])
+            return storage
+
+        # CSRF API methods
+        @manage_accessed
+        def new_csrf_token(self):
+            token = os.urandom(20).encode('hex')
+            self['_csrft_'] = token
+            return token
+
+        @manage_accessed
+        def get_csrf_token(self):
+            token = self.get('_csrft_', None)
+            if token is None:
+                token = self.new_csrf_token()
+            return token
+
+        # non-API methods
+        def _set_cookie(self, response):
+            if not self._cookie_on_exception:
+                exception = getattr(self.request, 'exception', None)
+                if exception is not None:  # don't set cookie during exceptions
+                    return False
+
+            self.save()
+            cookieval = signed_serialize(
+                (self.accessed, self.created, self['_id']), self._secret
+                )
+            response.set_cookie(
+                self._cookie_name,
+                value=cookieval,
+                max_age=self._cookie_max_age,
+                path=self._cookie_path,
+                domain=self._cookie_domain,
+                secure=self._cookie_secure,
+                httponly=self._cookie_httponly,
+                )
+            return True
+
+    return LuminSessionFactory
---
+++ lumin/tests/test_session.py
@@ -0,0 +1,427 @@
+import unittest
+from pyramid import testing
+
+
+class LuminSession(unittest.TestCase):
+    def _makeOne(self, request, **kw):
+        from lumin.session import LuminSessionFactoryConfig
+        return LuminSessionFactoryConfig('secret', **kw)(request)
+
+    def test_ctor_no_cookie(self):
+        from pymongo.objectid import ObjectId
+        request = testing.DummyRequest()
+        request.registry.settings = {}
+        request.db = DummyDB()
+        session = self._makeOne(request)
+        self.assertEqual(type(dict(session)['_id']), ObjectId)
+        self.assertEqual(len(dict(session)), 1)
+
+    def test_instance_conforms(self):
+        from zope.interface.verify import verifyObject
+        from pyramid.interfaces import ISession
+        request = testing.DummyRequest()
+        request.registry.settings = {}
+        request.db = DummyDB()
+        session = self._makeOne(request)
+        verifyObject(ISession, session)
+
+    def _serialize(self, accessed, state, secret='secret'):
+        from pyramid.session import signed_serialize
+        return signed_serialize((accessed, accessed, state), secret)
+
+    def test_ctor_with_cookie_still_valid(self):
+        import time
+        request = testing.DummyRequest()
+        request.registry.settings = {}
+        request.db = DummyDB()
+        cookieval = self._serialize(time.time(), 1)
+        request.cookies['lumin_session'] = cookieval
+        session = self._makeOne(request)
+        self.assertEqual(dict(session), {'_id': 1})
+
+    def test_ctor_with_cookie_expired(self):
+        from pymongo.objectid import ObjectId
+        request = testing.DummyRequest()
+        request.registry.settings = {}
+        request.db = DummyDB()
+        cookieval = self._serialize(0, 1)
+        request.cookies['lumin_session'] = cookieval
+        session = self._makeOne(request)
+        self.assertEqual(type(dict(session)['_id']), ObjectId)
+
+    def test_ctor_with_bad_cookie(self):
+        from pymongo.objectid import ObjectId
+        request = testing.DummyRequest()
+        request.registry.settings = {}
+        request.db = DummyDB()
+        cookieval = 'abc'
+        request.cookies['lumin_session'] = cookieval
+        session = self._makeOne(request)
+        self.assertEqual(type(dict(session)['_id']), ObjectId)
+        self.assertEqual(len(dict(session)), 1)
+
+    def test_not_changed(self):
+        request = testing.DummyRequest()
+        request.registry.settings = {}
+        request.db = DummyDB()
+        session = self._makeOne(request)
+        self.assertEqual(session.changed(), False)
+
+    def test_changed(self):
+        request = testing.DummyRequest()
+        request.registry.settings = {}
+        request.db = DummyDB()
+        session = self._makeOne(request)
+        session._dirty = True
+        self.assertEqual(session.changed(), True)
+
+    def test_invalidate(self):
+        request = testing.DummyRequest()
+        request.registry.settings = {}
+        request.db = DummyDB()
+        session = self._makeOne(request)
+        session['a'] = 1
+        self.assertEqual(session.invalidate(), None)
+        self.assertFalse('a' in session)
+
+    def test__set_cookie_on_exception(self):
+        request = testing.DummyRequest()
+        request.exception = True
+        request.registry.settings = {}
+        request.db = DummyDB()
+        session = self._makeOne(request)
+        session._cookie_on_exception = False
+        response = DummyResponse()
+        self.assertEqual(session._set_cookie(response), False)
+
+    def test__set_cookie_on_exception_no_request_exception(self):
+        import webob
+        request = testing.DummyRequest()
+        request.registry.settings = {}
+        request.db = DummyDB()
+        request.exception = None
+        session = self._makeOne(request)
+        session._cookie_on_exception = False
+        response = webob.Response()
+        self.assertEqual(session._set_cookie(response), True)
+        self.assertEqual(response.headerlist[-1][0], 'Set-Cookie')
+
+    def test__set_cookie_real_webob_response(self):
+        import webob
+        request = testing.DummyRequest()
+        request.registry.settings = {}
+        request.db = DummyDB()
+        session = self._makeOne(request)
+        session['abc'] = 'x'
+        response = webob.Response()
+        self.assertEqual(session._set_cookie(response), True)
+        self.assertEqual(response.headerlist[-1][0], 'Set-Cookie')
+
+    def test__set_cookie_options(self):
+        from pyramid.response import Response
+        request = testing.DummyRequest()
+        request.exception = None
+        request.registry.settings = {}
+        request.db = DummyDB()
+        session = self._makeOne(request,
+                                cookie_name='abc',
+                                cookie_path='/foo',
+                                cookie_domain='localhost',
+                                cookie_secure=True,
+                                cookie_httponly=True,
+                                )
+        session['abc'] = 'x'
+        response = Response()
+        self.assertEqual(session._set_cookie(response), True)
+        cookieval = response.headerlist[-1][1]
+        val, domain, path, secure, httponly = [x.strip() for x in
+                                               cookieval.split(';')]
+        self.assertTrue(val.startswith('abc='))
+        self.assertEqual(domain, 'Domain=localhost')
+        self.assertEqual(path, 'Path=/foo')
+        self.assertEqual(secure, 'secure')
+        self.assertEqual(httponly, 'HttpOnly')
+
+    def test_flash_default(self):
+        request = testing.DummyRequest()
+        request.registry.settings = {}
+        request.db = DummyDB()
+        session = self._makeOne(request)
+        session.flash('msg1')
+        session.flash('msg2')
+        self.assertEqual(session['_f_'], ['msg1', 'msg2'])
+
+    def test_flash_allow_duplicate_false(self):
+        request = testing.DummyRequest()
+        request.registry.settings = {}
+        request.db = DummyDB()
+        session = self._makeOne(request)
+        session.flash('msg1')
+        session.flash('msg1', allow_duplicate=False)
+        self.assertEqual(session['_f_'], ['msg1'])
+
+    def test_flash_allow_duplicate_true_and_msg_not_in_storage(self):
+        request = testing.DummyRequest()
+        request.registry.settings = {}
+        request.db = DummyDB()
+        session = self._makeOne(request)
+        session.flash('msg1', allow_duplicate=True)
+        self.assertEqual(session['_f_'], ['msg1'])
+
+    def test_flash_allow_duplicate_false_and_msg_not_in_storage(self):
+        request = testing.DummyRequest()
+        request.registry.settings = {}
+        request.db = DummyDB()
+        session = self._makeOne(request)
+        session.flash('msg1', allow_duplicate=False)
+        self.assertEqual(session['_f_'], ['msg1'])
+
+    def test_flash_mixed(self):
+        request = testing.DummyRequest()
+        request.registry.settings = {}
+        request.db = DummyDB()
+        session = self._makeOne(request)
+        session.flash('warn1', 'warn')
+        session.flash('warn2', 'warn')
+        session.flash('err1', 'error')
+        session.flash('err2', 'error')
+        self.assertEqual(session['_f_warn'], ['warn1', 'warn2'])
+
+    def test_pop_flash_default_queue(self):
+        request = testing.DummyRequest()
+        request.registry.settings = {}
+        request.db = DummyDB()
+        session = self._makeOne(request)
+        queue = ['one', 'two']
+        session['_f_'] = queue
+        result = session.pop_flash()
+        self.assertEqual(result, queue)
+        self.assertEqual(session.get('_f_'), None)
+
+    def test_pop_flash_nodefault_queue(self):
+        request = testing.DummyRequest()
+        request.registry.settings = {}
+        request.db = DummyDB()
+        session = self._makeOne(request)
+        queue = ['one', 'two']
+        session['_f_error'] = queue
+        result = session.pop_flash('error')
+        self.assertEqual(result, queue)
+        self.assertEqual(session.get('_f_error'), None)
+
+    def test_peek_flash_default_queue(self):
+        request = testing.DummyRequest()
+        request.registry.settings = {}
+        request.db = DummyDB()
+        session = self._makeOne(request)
+        queue = ['one', 'two']
+        session['_f_'] = queue
+        result = session.peek_flash()
+        self.assertEqual(result, queue)
+        self.assertEqual(session.get('_f_'), queue)
+
+    def test_peek_flash_nodefault_queue(self):
+        request = testing.DummyRequest()
+        request.registry.settings = {}
+        request.db = DummyDB()
+        session = self._makeOne(request)
+        queue = ['one', 'two']
+        session['_f_error'] = queue
+        result = session.peek_flash('error')
+        self.assertEqual(result, queue)
+        self.assertEqual(session.get('_f_error'), queue)
+
+    def test_new_csrf_token(self):
+        request = testing.DummyRequest()
+        request.registry.settings = {}
+        request.db = DummyDB()
+        session = self._makeOne(request)
+        token = session.new_csrf_token()
+        self.assertEqual(token, session['_csrft_'])
+
+    def test_get_csrf_token(self):
+        request = testing.DummyRequest()
+        request.registry.settings = {}
+        request.db = DummyDB()
+        session = self._makeOne(request)
+        session['_csrft_'] = 'token'
+        token = session.get_csrf_token()
+        self.assertEqual(token, 'token')
+        self.assertTrue('_csrft_' in session)
+
+    def test_get_csrf_token_new(self):
+        request = testing.DummyRequest()
+        request.registry.settings = {}
+        request.db = DummyDB()
+        session = self._makeOne(request)
+        token = session.get_csrf_token()
+        self.assertTrue(token)
+        self.assertTrue('_csrft_' in session)
+
+
+class Test_manage_accessed(unittest.TestCase):
+    def _makeOne(self, wrapped):
+        from pyramid.session import manage_accessed
+        return manage_accessed(wrapped)
+
+    def test_accessed_set(self):
+        request = testing.DummyRequest()
+        session = DummySessionFactory(request)
+        session.accessed = None
+        wrapper = self._makeOne(session.__class__.get)
+        wrapper(session, 'a')
+        self.assertNotEqual(session.accessed, None)
+
+    def test_already_dirty(self):
+        request = testing.DummyRequest()
+        session = DummySessionFactory(request)
+        session._dirty = True
+        session['a'] = 1
+        wrapper = self._makeOne(session.__class__.get)
+        self.assertEqual(wrapper.__doc__, session.get.__doc__)
+        result = wrapper(session, 'a')
+        self.assertEqual(result, 1)
+        callbacks = request.response_callbacks
+        self.assertEqual(len(callbacks), 0)
+
+    def test_with_exception(self):
+        import webob
+        request = testing.DummyRequest()
+        request.exception = True
+        session = DummySessionFactory(request)
+        session['a'] = 1
+        wrapper = self._makeOne(session.__class__.get)
+        self.assertEqual(wrapper.__doc__, session.get.__doc__)
+        result = wrapper(session, 'a')
+        self.assertEqual(result, 1)
+        callbacks = request.response_callbacks
+        self.assertEqual(len(callbacks), 1)
+        response = webob.Response()
+        result = callbacks[0](request, response)
+        self.assertEqual(result, None)
+        self.assertFalse('Set-Cookie' in dict(response.headerlist))
+
+    def test_cookie_is_set(self):
+        request = testing.DummyRequest()
+        session = DummySessionFactory(request)
+        session['a'] = 1
+        wrapper = self._makeOne(session.__class__.get)
+        self.assertEqual(wrapper.__doc__, session.get.__doc__)
+        result = wrapper(session, 'a')
+        self.assertEqual(result, 1)
+        callbacks = request.response_callbacks
+        self.assertEqual(len(callbacks), 1)
+        response = DummyResponse()
+        result = callbacks[0](request, response)
+        self.assertEqual(result, None)
+        self.assertEqual(session.response, response)
+
+
+def serialize(data, secret):
+    try:
+        from hashlib import sha1
+    except ImportError:  # pragma: no cover
+        import sha as sha1
+
+    try:
+        import cPickle as pickle
+    except ImportError:  # pragma: no cover
+        import pickle
+
+    import hmac
+    import base64
+    pickled = pickle.dumps('123', pickle.HIGHEST_PROTOCOL)
+    sig = hmac.new(secret, pickled, sha1).hexdigest()
+    return sig + base64.standard_b64encode(pickled)
+
+
+class Test_signed_serialize(unittest.TestCase):
+    def _callFUT(self, data, secret):
+        from pyramid.session import signed_serialize
+        return signed_serialize(data, secret)
+
+    def test_it(self):
+        expected = serialize('123', 'secret')
+        result = self._callFUT('123', 'secret')
+        self.assertEqual(result, expected)
+
+
+class Test_signed_deserialize(unittest.TestCase):
+    def _callFUT(self, serialized, secret, hmac=None):
+        if hmac is None:
+            import hmac
+        from pyramid.session import signed_deserialize
+        return signed_deserialize(serialized, secret, hmac=hmac)
+
+    def test_it(self):
+        serialized = serialize('123', 'secret')
+        result = self._callFUT(serialized, 'secret')
+        self.assertEqual(result, '123')
+
+    def test_invalid_bits(self):
+        serialized = serialize('123', 'secret')
+        self.assertRaises(ValueError, self._callFUT, serialized, 'seekrit')
+
+    def test_invalid_len(self):
+        class hmac(object):
+            def new(self, *arg):
+                return self
+
+            def hexdigest(self):
+                return '1234'
+        serialized = serialize('123', 'secret123')
+        self.assertRaises(ValueError, self._callFUT, serialized, 'secret',
+                          hmac=hmac())
+
+    def test_it_bad_encoding(self):
+        serialized = 'bad' + serialize('123', 'secret')
+        self.assertRaises(ValueError, self._callFUT, serialized, 'secret')
+
+
+class DummySessionFactory(dict):
+    _dirty = False
+    _cookie_name = 'lumin_session'
+    _cookie_max_age = None
+    _cookie_path = '/'
+    _cookie_domain = None
+    _cookie_secure = False
+    _cookie_httponly = False
+    _timeout = 1200
+    _secret = 'secret'
+
+    def __init__(self, request):
+        self.request = request
+        dict.__init__(self, {})
+
+    def _set_cookie(self, response):
+        self.response = response
+
+
+class DummyResponse(object):
+    def __init__(self):
+        self.headerlist = []
+
+
+class DummyCollection(object):
+    def __init__(self, find_result):
+        self.find_result = find_result
+
+    def find_one(self, spec):
+        return spec
+
+    def remove(self, spec):
+        return spec['_id']
+
+    def save(self, spec):
+        return spec['_id']
+
+
+class Database(dict):
+    pass
+
+
+class DummyDB(Database):
+    def __init__(self, find_result=None):
+        self['lumin.sessions'] = DummyCollection(find_result=find_result)
+        self['tempstore'] = DummyCollection(find_result=find_result)
+        dict.__init__(self)
