43c764c8caa1833594d92aeac3396401777b122b
Josh Finnie <josh@koansys.com>
2012-11-20 11:27:00 -0500

    Adding test for Collection
    Starting tests for collection.

--- lumin/tests/test_node.py
+++ lumin/tests/test_node.py
@@ -1,6 +1,8 @@
 from __future__ import unicode_literals
 import unittest
 
+import mongomock
+
 import pyramid.testing
 
 
@@ -37,3 +39,31 @@ class TestFactory(unittest.TestCase):
     def test_factory_parent(self):
         result = self._call_fut(request=self.request)
         self.assertEquals(result.__parent__, None)
+
+
+class TestCollection(unittest.TestCase):
+    def setUp(self):
+        self.config = pyramid.testing.setUp()
+        self.request = pyramid.testing.DummyRequest()
+
+        self.mock_conn = mongomock.Connection()
+        self.request.db = mongomock.Database(self.mock_conn)
+
+    def tearDown(self):
+        pyramid.testing.tearDown()
+
+    def _call_fut(self, request=None, name=None):
+        from lumin.node import Collection
+        return Collection(request=request, name=name)
+
+    def test_collection_setup_no_name(self):
+        result = self._call_fut(request=self.request)
+        self.assertEquals(result.__name__, None)
+        self.assertEquals(result._collection.count(), 0)
+        self.assertEquals(result._collection_history.count(), 0)
+
+    def test_collection_setup_with_name(self):
+        result = self._call_fut(request=self.request, name="Test_Name")
+        self.assertEquals(result.__name__, "Test_Name")
+        self.assertEquals(result._collection.count(), 0)
+        self.assertEquals(result._collection_history.count(), 0)
